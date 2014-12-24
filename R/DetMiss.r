9990d5c4b32538181c8cd6fa552989f0ace7756f
SixiangHu <Sixiang.Hu@gmail.com>
2014-12-24 00:02:50 +0000

    first commit

---
+++ R/DetMiss.r
@@ -0,0 +1,63 @@
+#' Detect Missing Value
+#'
+#' @description This function allows you to detect missing values in given dataset. 
+#' @usage DetMiss(data)
+#' @param data This could be data frame, matrix or a vector.
+#' @keywords Missing Value
+#' @export A data frame with summary info about missing value
+#' @seealso DetMiss
+#' @author Sixiang.Hu.at.Gmail.Com
+#' @examples
+#' a <- c(sample(LETTERS,5),NA)
+#' DetMiss(a)
+
+if(getRversion() < "3.1.0") anyNA <- function(x) any(is.na(x))
+
+setGeneric("DetMiss",function(data) standardGeneric("DetMiss"))
+
+DetMiss.matrix <- function(data){
+	v <- DetMiss(as.vector(data))
+}
+
+DetMiss.factor<-function(data){
+  if(length(data)==0) stop(paste("object '",deparse(substitute(data)),"' blank or not found.\n"))
+  RetTab<-data.frame(class(data),sum(is.na(data)),stringsAsFactors=FALSE)
+  colnames(RetTab) <- c("    Variable Class","    Num of NA")
+  return(RetTab)
+}
+
+DetMiss.data.frame<-function(data){
+  
+  RetTab<-data.frame()
+  
+  if(dim(data)[1]==0) stop(paste("object '",deparse(substitute(data)),"' blank or not found.\n"))
+  
+  AllMiss <- which(sapply(data,anyNA))
+  Names <- names(AllMiss)
+  iLen <- length(Names)
+
+  if(iLen==0 ){
+    cat("No missing value found.\n")
+    return()
+  }
+  
+  for(i in 1:iLen){
+    var_lng<-length(data[is.na(data[,Names[i]]),Names[i]])
+    tmp<-data.frame(AllMiss[[i]],Names[i],class(data[,Names[i]]),var_lng,stringsAsFactors=FALSE)
+    RetTab<-rbind(RetTab,tmp)
+  }
+
+  colnames(RetTab) <- c("Variable Column","    Variable Name","    Variable Class","    Num of NA")
+  RetTab <- RetTab[order(RetTab[,"    Num of NA"],rev(RetTab[,"    Variable Name"]),decreasing=TRUE),]
+  return(RetTab)
+
+}
+
+setMethod("DetMiss",signature(data="factor"),DetMiss.factor,valueClass="factor")
+setMethod("DetMiss",signature(data="logical"),DetMiss.factor,valueClass="logical")
+setMethod("DetMiss",signature(data="Date"),DetMiss.factor,valueClass="Date")
+setMethod("DetMiss",signature(data="character"),DetMiss.factor,valueClass="character")
+setMethod("DetMiss",signature(data="integer"),DetMiss.factor,valueClass="integer")
+setMethod("DetMiss",signature(data="numeric"),DetMiss.factor,valueClass="numeric")
+setMethod("DetMiss",signature(data="data.frame"),DetMiss.data.frame,valueClass="data.frame")
+setMethod("DetMiss",signature(data="matrix"),DetMiss.matrix,valueClass="numeric")
---
+++ R/PopMiss.r
@@ -0,0 +1,122 @@
+#' Populate Missing Value
+#'
+#' @description This function allows you to populate missing values. 
+#' @usage PopMiss(data)
+#' @param data This could be data frame, matrix or a vector.
+#' @param na.treatment One of "mean.or.mode", "delete", or "replace", to specify the method to populate missing values.
+#' @param replace A single value used for populating ALL missing value.  May not be useful for a data frame with missing values in different type of variable. 
+#' @keywords Missing Value
+#' @export A data frame or vector with missing value being populated
+#' @seealso DetMiss
+#' @author Sixiang.Hu.at.Gmail.Com
+#' @examples
+#' a <- c(sample(LETTERS,5),NA)
+#' PopMiss(a)
+
+
+if(getRversion() < "3.1.0") anyNA <- function(x) any(is.na(x))
+
+setGeneric("PopMiss",function(data,
+                              na.treatment=c("mean.or.mode","delete","replace"),
+                              replace=NULL)
+           standardGeneric("PopMiss")
+  )
+
+PopMiss.factor<-function(data,na.treatment,replace){
+  num_miss <- length(which(is.na(data)))
+  na.treatment <- match.arg(na.treatment)
+  if(isTRUE(na.treatment=="delete")) {
+    cat(paste(num_miss," rows have been deleted.\n\n"))
+    return(na.omit(data))
+  }
+  else if(isTRUE(na.treatment=="mean.or.mode")){
+    replace <- names(table(data))[order(table(data),decreasing=TRUE)[1]]
+  }
+  
+  if (is.null(replace)) stop("NULL value used for populating NAs. Chosen \"replace\" method, but didn't provide a value for replace parameter?")
+  data[is.na(data)] <- replace
+  cat(paste(num_miss," row have been populated by value: \"",replace,"\".\n\n"))
+  return(data)
+}
+
+PopMiss.integer<-function(data,na.treatment,replace){
+  num_miss <- length(which(is.na(data)))
+  na.treatment <- match.arg(na.treatment)
+  if(isTRUE(na.treatment=="delete")) {
+    cat(paste(num_miss," rows have been delete.\n\n"))
+        return(na.omit(data))
+  }
+  else if(isTRUE(na.treatment=="mean.or.mode")){
+    replace <- floor(mean(data,na.rm=TRUE))
+  }
+  if (is.null(replace)) stop("NULL value used for populating NAs. Chosen \"replace\" method, but didn't provide a value for replace parameter?")
+  data[is.na(data)] <- replace
+  cat(paste(num_miss," row have been populated by value:",replace,".\n\n"))
+  return(data)
+}
+
+PopMiss.numeric<-function(data,na.treatment,replace){
+  num_miss <- length(which(is.na(data)))
+  na.treatment <- match.arg(na.treatment)
+  if(isTRUE(na.treatment=="delete")) {
+    cat(paste(num_miss," rows have been delete.\n\n"))
+        return(na.omit(data))
+  }
+  else if(isTRUE(na.treatment=="mean.or.mode")){
+    replace <- mean(data,na.rm=TRUE)
+  }
+  if (is.null(replace)) stop("NULL value used for populating NAs. Chosen \"replace\" method, but didn't provide a value for replace parameter?")
+  
+  data[is.na(data)] <- replace
+  cat(paste(num_miss," row have been populated by value:",replace,".\n\n"))
+  return(data)
+}
+
+PopMiss.data.frame<-function(data,na.treatment,replace){
+  na.treatment <- match.arg(na.treatment)
+  num_miss <- which(sapply(data,anyNA))
+  if(isTRUE(na.treatment=="delete")) {
+    cat(paste(num_miss," columns have been cleaned.\n\n"))
+    return(na.omit(data))
+  }
+  else if(isTRUE(na.treatment=="mean.or.mode")){
+    str_name <- names(data)
+    for(i in num_miss){
+      if(class(data[,str_name[i]]) %in% c("character","date","factor"))
+        replace <- names(table(data[,str_name[i]]))[order(table(data[,str_name[i]]),decreasing=TRUE)[1]]
+      else if(class(data[,str_name[i]])=="integer" )
+        replace <- floor(mean(data[,str_name[i]],na.rm=TRUE))
+      else 
+        replace <- mean(data[,str_name[i]],na.rm=TRUE)
+      data[is.na(data[,str_name[i]]),str_name[i]] <- replace
+    }
+    
+    cat(paste(num_miss," columns have been populated with mean or mode.\n\n"))
+    return(data)
+  }
+  else{
+    if (is.null(replace)) stop("NULL value used for populating NAs. Chosen \"replace\" method, but didn't provide a value for replace parameter?")
+    
+    warning("Provided replacement will be used for all missing value.\n\n")
+    str_name <- names(data)
+    for(i in num_miss)
+      data[is.na(data[,str_name[i]]),str_name[i]] <- replace
+    cat(paste(num_miss," columns have been populated with mean or mode.\n\n"))
+    return(data)
+  }
+}
+
+PopMiss.matrix <- function(data,na.treatment,replace){
+	if(identical(match.arg(na.treatment),"delete")) warning("na.treatment is \"delete\" and 'data' is a matrix. This only works correctly if whole rows are missing")
+	matrix(PopMiss(as.vector(data),na.treatment,replace),ncol=ncol(data))
+}
+
+setMethod("PopMiss",signature(data="factor"),PopMiss.factor)
+setMethod("PopMiss",signature(data="character"),PopMiss.factor)
+setMethod("PopMiss",signature(data="Date"),PopMiss.factor)
+setMethod("PopMiss",signature(data="logical"),PopMiss.factor)
+setMethod("PopMiss",signature(data="integer"),PopMiss.integer)
+setMethod("PopMiss",signature(data="numeric"),PopMiss.numeric)
+setMethod("PopMiss",signature(data="data.frame"),PopMiss.data.frame)
+setMethod("PopMiss",signature(data="matrix"),PopMiss.matrix)
+
