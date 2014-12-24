a4f629460a67959a3a0e7fe6711e86b8d3cb3c53
SixiangHu <Sixiang.Hu@gmail.com>
2014-12-24 00:16:31 +0000

    first commit - Data Summary

---
+++ R/DataSummary.R
@@ -0,0 +1,54 @@
+#' DataSummary
+#'
+#' @description This function allows you to have a summary about the given dataset.
+#' @usage DataSummary(data,weights=NULL)
+#' @param data This could be data frame or a vector.
+#' @keywords Data Summary
+#' @export A data frame with summary info including min, max, missing value counts, and mean or mode for each variable in the dataset.
+#' @author Sixiang.Hu.at.Gmail.Com
+#' @examples
+#' DataSummary(cars)
+
+DataSummary <- function(data,weights=NULL){
+  if(is.null(weights)) weights <- rep(1,nrow(data))
+  
+  dsName    <- names(data)
+  dsClass   <- sapply(data,class)
+  dsNLevels <- sapply(data,function(x) nlevels(as.factor(x)))
+  dsMiss    <- sapply(data,function(x) sum(is.na(x)))
+  
+  dsMean    <- sapply(data,function(x){
+    if(is.numeric(x) || is.integer(x)) as.character(round(weighted.mean(x,weights,na.rm = TRUE),6))
+    else {
+      x.dt<-data.table(x,weights)
+      as.character(x.dt[,sum(weights),by=x][order(-V1)][1,list(x)])
+    }
+  }
+  )
+  dsMax    <- sapply(data,function(x){
+    if(is.numeric(x) || is.integer(x)) as.character(round(max(x,na.rm = TRUE),6))
+    else {
+      x.dt<-data.table(x,weights)
+      as.character(x.dt[,sum(weights),by=x][order(-V1)][1,list(x)])
+    }
+  }
+  )
+  dsMin    <- sapply(data,function(x){
+    if(is.numeric(x) || is.integer(x)) as.character(round(min(x,na.rm = TRUE),6))
+    else {
+      x.dt<-data.table(x,weights)
+      as.character(x.dt[,sum(weights),by=x][order(V1)][1,list(x)])
+    }
+  }
+  )
+  
+  return(data.frame("Variable Name"=dsName,
+                    "Variable Type"=dsClass,
+                    "Number of Unique Value"=dsNLevels,
+                    "Number of Missing value"=dsMiss,
+                    "Mean or Mode"=unlist(dsMean),
+                    "Min"=unlist(dsMin),
+                    "Max"=unlist(dsMax)
+  )
+  )
+}
