4c4e9cade156a9b2c5c8db490792e1dc4245fe36
SixiangHu <Sixiang.Hu@Gmail.com>
2015-05-31 22:47:09 +0100

    modelPlot: change example

--- R/modelPlot.r
+++ R/modelPlot.r
@@ -32,51 +32,11 @@
 #' @export modelPlot
 #' @examples
 #' 
-#' ## Not run:
-#' #example from gbm package
-#' N <- 1000
-#' X1 <- runif(N)
-#' X2 <- 2*runif(N)
-#' X3 <- ordered(sample(letters[1:4],N,replace=TRUE),levels=letters[4:1])
-#' X4 <- factor(sample(letters[1:6],N,replace=TRUE))
-#' X5 <- factor(sample(letters[1:3],N,replace=TRUE))
-#' X6 <- 3*runif(N) 
-#' mu <- c(-1,0,1,2)[as.numeric(X3)]
-#' 
-#' SNR <- 10 
-#' Y <- X1**1.5 + 2 * (X2**.5) + mu
-#' sigma <- sqrt(var(Y)/SNR)
-#' Y <- Y + rnorm(N,0,sigma)
-#' 
-#' X1[sample(1:N,size=500)] <- NA
-#' X4[sample(1:N,size=300)] <- NA
-#' 
-#' data_raw <- data.frame(Y=Y,X1=X1,X2=X2,X3=X3,X4=X4,X5=X5,X6=X6)
-#' 
-#' gbm1 <-
-#' gbm(Y~X1+X2+X3+X4+X5+X6,         
-#'     data=data_raw,                   
-#'     var.monotone=c(0,0,0,0,0,0), 
-#'     distribution="gaussian",     
-#'     n.trees=1000,                
-#'     shrinkage=0.05,              
-#'     interaction.depth=3,         
-#'     bag.fraction = 0.5,          
-#'     train.fraction = 0.5,        
-#'     n.minobsinnode = 10,         
-#'     cv.folds = 3,                
-#'     keep.data=TRUE,              
-#'     verbose=FALSE,               
-#'     n.cores=1)
-#' 
-#' modelPlot(gbm1,xvar="X3",modelType="gbm",by="X5")
-#' 
-#' #a glm example
+#' glm example
 #' 
 #' glm1 <- glm(formula = mpg ~ cyl + hp, family = Gamma(log), data = mtcars, weights = wt)
 #' 
 #' modelPlot(glm1,"cyl",modelType="glm",interactive=TRUE)
-#' ## End(Not run)
 
 modelPlot <- function(model,
                       xvar,
