1998ce83cf646df3ba2fd6c231b9a29e41c9895d
Sixiang Hu <Sixiang.Hu@Gmail.com>
2018-04-13 11:21:31 +0100

    Update dataPlot.r
    - turn to `plotly` as plot engine
    - more efficient
    - better visual relationship between histogram and line

--- R/dataPlot.r
+++ R/dataPlot.r
@@ -1,141 +1,171 @@
-#' dataPlot
+#' dataPlot: visualise observation
 #'
 #' @description This function allows you to visualise features of a dataset 
 #' by specifying dependent and response variable.
-#' @usage dataPlot(data,xvar,yvar,byvar=NULL,weights=NULL,newGroupNum=10)
-#' @param data a data frame.
-#' @param xvar either an integer to specify the position of the dependent variable in the data frame, 
-#' or a character string to indicate the dependent variable name.
-#' @param yvar either an integer to specify the position of the response variable in the data frame, 
-#' or a character string to indicate the response variable name.
-#' @param weights Optional. One of: 
-#' a numerical vector to specify the weights used for calculating weighted average of response,
-#' a character string to specify the name of weight variable in the data frame,
-#' an integer to specify the position of the weight variable in the data frame.
-#' @param byvar Optinal. either an integer to specify the position of the <by> variable in the data frame, 
-#' or a character string to indicate the <by> variable name.
+#' @usage dataPlot(x,y,by=NULL,weights=NULL, exposure = NULL,newGroupNum=10, xlim=NULL)
+#' @param x a vector indicates the dependent variable
+#' @param y a vector indicates the response variable
+#' @param weights Optional. A numerical vector to specify the weights used for calculating weighted average of response.
+#' normally this is the figures from over/down-samping.
+#' @param exposure Optional. A numerical vector to specify the exposure used for calculating weighted average of response.
+#' @param by Optinal.A numerical vector to specify the <by> variable
 #' @param newGroupNum An integer specifies number of new bands 
-#' when levels of current plotting variable `xvar` or `by` is more than 100.
-#' @param legend_x,legend_y numerical value. give the position of the legend
-#' @param x_maxBins,by_maxBins integer. If number of unique level over this value, than a banding will be triggered.
+#' when levels of current plotting variable `xvar` or `by` is more than 100. 
+#' @param xlim Optional. A vector provides the range of the variable e.g. xlim=c(0,100).
+#' @param breaks Optional. A vector to specify the breaks for the xvar.
+#' @param missing logical. whether to show the `NA` as `Missing` in plot.  If a `Missing` level is already existed, then `NA` will combined
+#' @param xname
+#' @param yname character. Name on the y-axis. By default: `Response`
+#' @param byname Optional. Characters to be shown on plot.
 #' @details 
 #' Before entering modelling stage, we may want to go through variable by variable in a data set to find the 
 #' features for response variable. This function provides this functionality.
 #' 
-#' \code{\link{modelPlot}} function in this package provides similar function, but \code{\link{modelPlot}} can 
-#' only visualise the feature in a specific model. 
-#' What's more, \code{\link{modelPlot}} takes longer time to create plots.
+#' \code{\link{compPlot}} function in this package provides similar function, 
+#' but \code{\link{compPlot}} provides additional capability to visualise model prediction along side of observation.
 #'  
-#' @author Sixiang Hu
-#' @importFrom data.table as.data.table data.table setkey :=
-#' @importFrom plotly plot_ly add_trace layout
-#' @export dataPlot
+#' What's more, \code{\link{compPlot}} takes longer time to create plots.
+#'  
+#' @author Sixiang.Hu
+#' 
+#' @importFrom data.table as.data.table data.table setkey := uniqueN
+#' @importFrom plotly plot_ly add_lines add_markers add_bars layout %>%
+#' @export
 #' @examples
 #' 
-#' dataPlot(mtcars,"wt","mpg")
+#' dataPlot(mtcars$gear,mtcars$mpg)
 #' 
-#' dataPlot(mtcars,"wt","mpg",byvar="vs")
+#' dataPlot(mtcars$gear,mtcars$mpg,mtcars$wt)
 
-dataPlot <- function(data,xvar,yvar,byvar=NULL,weights=NULL,
-                     newGroupNum=10, legend_x= 0.45, legend_y = -0.1,
-                     x_maxBins = 100, by_maxBins = 20){
+dataPlot <- function(x,y,by=NULL,weights=NULL, exposure = NULL,newGroupNum=10, xlim=NULL, breaks = NULL, missing=TRUE,
+                      xname="x",yname="Response",byname="by"){
   
   # Error Trap
-  if( .isDFnull(data) ) stop("data set provided is null.")
-  if( is.null(xvar) ) stop("X variable provided is null.") 
-  if( is.null(yvar) ) stop("Y variable provided is null.")
+  if( is.null(x)  ) stop("xvar provided is null.") 
+  if( is.null(y)  ) stop("yvar provided is null.")
   
   # Find data column
-  posi <- .VarPosition(data,xvar)
-  x <- data[[posi$posi]]
-  xname <- posi$name
-  
-  posi <- .VarPosition(data,yvar)
-  y <- data[[posi$posi]]
-  yname <- posi$name
-  
-  if( !is.null(byvar) ){
-    posi <- .VarPosition(data,byvar)
-    by <- data[[posi$posi]]
-    byname <- posi$name
+  if (length(x) != length(y)) stop("x and y don't have the same length")
+
+  if( !is.null(by) ){
+    if (length(x) != length(by)) stop("x and by don't have the same length")
   }
-  else by <- NULL
+
+  # range index
+  if ( is.null(xlim) ) {
+    ind <- 1:length(x)
+  }
+  else if ( !is.null(xlim) && !(is.numeric(x) || is.integer(x) || is.date(x)) ) {
+    ind <- 1:length(x)
+    warning("xlim is provided on character variable. Ignored")
+  }
+  else if (length(xlim)>2 || length(xlim)<2) {
+    ind <- 1:length(x)
+    warning("xlim provided must has 2 elements.")
+  }
+  else ind <- which(x>=xlim[1] & x<=xlim[2])
+  
   
   # weights
   if( !is.null(weights) ){
-    if (is.integer(weights) && length(weights)>1){
-      if ( dim(data)[1] != length(weights) ) stop ("Length of weights is not the same as dimension of the data provided.")
-      w <- weights
-    }
-    else{
-      posi <- .VarPosition(data,weights)
-      w <- data[[posi$posi]]
-    }
-  }
-  else w <- rep(1,dim(data)[1])
+    if (length(x) != length(weights)) 
+      stop("x and weights don't have the same length")
+    else
+      w <- weights[ind]
+  }else w <- rep(1,length(ind))
   
-  #New group for xvar if it has too many levels.
-  if ( (is.numeric(x) || is.integer(x) ) && nlevels(as.factor(x))>=x_maxBins ) {
-    new_band <- dmBreak(x,newGroupNum)
-    x <- cut(x,new_band,include.lowest = TRUE,ordered_result=TRUE)
-  }
+  if( !is.null(exposure) ){
+    if (length(x) != length(exposure)) 
+      stop("x and weights don't have the same length")
+    else
+      e <- exposure[ind]
+  }else e <- rep(1,length(ind))
   
+  #New group for x if it has too many levels.
+  if(!is.null(breaks)){
+    x <- cut(x, breaks, include.lowest = TRUE, ordered_result = TRUE)
+  }else if (is.numeric(x) & nlevels(as.factor(x)) > 100) {
+    # new_band <- dmBreak(x, newGroupNum)
+    # x <- cut(x, new_band, include.lowest = TRUE, ordered_result = TRUE)
+    x <- niceBin(x,newGroupNum)
+  }
+
   #New Group for byvar if it has too many levels.
   if(!is.null(by)){
-    if ( (is.numeric(by) || is.integer(by)) && nlevels(as.factor(by))>=by_maxBins ) {
-      new_band <- dmBreak(by,newGroupNum)
-      by <- cut(by,new_band,include.lowest = TRUE,ordered_result=TRUE)
+    if ( (is.numeric(by) || is.integer(by)) && data.table::uniqueN(by[ind])>20 ) {
+      # new_band <- dmBreak(by[ind],newGroupNum)
+      # by <- cut(by[ind],new_band,include.lowest = TRUE,ordered_result=TRUE)
+      by <- niceBin(by,newGroupNum)
     }
-    if (is.numeric(by)) by <- as.factor(by)
+    if (is.numeric(by)) by <- by[ind]
   }
   
-  #Data for plot
+  #Title for plot
   strTitle <- paste("Observation Analysis on: ",xname)
-  
+
   #set axis
   ay1 <- list(overlaying = "y2", side = "left", title=yname, 
               linecolor = "#000000", gridcolor = "#E5E5E5")
   
-  ay2 <- list(side = "right", showgrid=FALSE, title="Weights",
+  ay2 <- list(side = "right", showgrid=FALSE, title="Weights(%)",
               linecolor = "#000000")
   
-  ax <- list(title=xname, showline=TRUE, linecolor = "#000000",
-             gridcolor = "#E5E5E5")
+  if(is.character(x)) {
+    ax <- list(title="", showline=TRUE, linecolor = "#000000",
+               gridcolor = "#E5E5E5",type = "category",
+               categoryorder = "category ascending")
+  }  else{
+    ax <- list(title="", showline=TRUE, linecolor = "#000000",
+               gridcolor = "#E5E5E5")
+  }
   
-  l <- list(bordercolor = "#000000",borderwidth=1,x = legend_x, y = legend_y)
-  
+  l <- list(bordercolor = "#000000",borderwidth=1,orientation="h")
+
   if (is.null(by)) {
-    data.plot <- data.table::data.table(x=x,y=y,w=w)
+    data.plot <- data.table::data.table(x=x[ind],y=y[ind],w=w[ind],e=e[ind])
     data.table::setkey(data.plot,x)
     
-    data.plot <- data.plot[,lapply(.SD,as.numeric),by=x,.SDcols=c("y","w")]
-    data.agg <- data.plot[,lapply(.SD,weighted.mean,w=w),by=x,.SDcols=c("y","w")]
-    data.hist <- data.plot[,sum(w),by=x][,freq:=V1/sum(V1)][order(x)]
+    if(missing){
+      set(data.plot,i=which(is.na(data.plot[["x"]])),j="x",value="Missing")
+    }
     
-    plotly::plot_ly(data=data.agg, x=x, y=y, line=list(color="#CC3399",shape="linear"),
-                    marker=list(symbol="square",size=10), name="Observed",yaxis="y1") %>%
-      plotly::add_trace(x=x,y=freq,data=data.hist,type="bar",showlegend=FALSE,
+    data.plot <- data.plot[,lapply(.SD,as.numeric),by=x,.SDcols=c("y","w","e")]
+    data.agg  <- data.plot[,lapply(.SD,function(x,w,e) sum(y*w,na.rm=TRUE)/sum(e*w,na.rm=TRUE),e=e,w=w),by=x,.SDcols=c("y","w","e")]
+    data.hist <- data.plot[,sum(e),by=x][,freq:=round(V1/sum(V1)*100,1)][order(x)]
+
+    resPlot <- plotly::plot_ly(data=data.agg, x=~x, y=~y, name="Observed") %>%
+      plotly::add_lines(line=list(color="#CC3399"),yaxis="y1") %>%
+      plotly::add_markers(marker=list(color="#CC3399",symbol="square",size=10),showlegend=FALSE) %>%
+      plotly::add_bars(x=~x,y=~freq,data=data.hist,showlegend=FALSE,
                         marker=list(color="#99CCFF",line=list(color="#606060",width=1.5)),
                         opacity=0.5,yaxis = "y2") %>%
-      plotly::layout(title=strTitle, xaxis=ax, yaxis=ay1, yaxis2=ay2, legend=l)
+      plotly::layout(title=strTitle, xaxis=ax, yaxis=ay1, yaxis2=c(ay2,list(range=c(0,min(max(data.hist$freq)*2.5,100)))), 
+                     legend=l, margin=list(l=-5,r=-5,t=-5,b=-5))
+    suppressWarnings(resPlot)
   }
   else{
-    strTitle <- paste(strTitle," by ",byname,sep="")
+    strTitle  <- paste(strTitle," by ",byname,sep="")
     
-    data.plot <- data.table::data.table(x=x,y=y,w=w,by=by)
+    data.plot <- data.table::data.table(x=x[ind],y=y[ind],w=w[ind],e=e[ind],by=by[ind])
     data.table::setkey(data.plot,x,by)
     
-    data.plot <- data.plot[,lapply(.SD,as.numeric),by=list(x,by),.SDcols=c("y","w")]
-    data.agg  <- data.plot[,lapply(.SD,weighted.mean,w=w),by=list(x,by),.SDcols=c("y","w")]
-    data.hist <- data.plot[,sum(w),by=list(x,by)][,freq:=V1/sum(V1)][order(by,x)]
+    if(missing){
+      set(data.plot,i=which(is.na(data.plot[["x"]])),j="x",value="Missing")
+      set(data.plot,i=which(is.na(data.plot[["by"]])),j="by",value="Missing")
+    }
     
-    suppressWarnings(
-      plotly::plot_ly(data=data.agg,x=x,y=y,color=paste("Observed",by,sep="-"),yaxis = "y1")%>%
-        plotly::add_trace(x=x, y=freq, color=by, data=data.hist, type="bar",
+    data.plot <- data.plot[,lapply(.SD,as.numeric),by=list(x,by),.SDcols=c("y","w","e")]
+    data.agg  <- data.plot[,lapply(.SD,function(x,w,e) sum(y*w,na.rm=TRUE)/sum(e*w,na.rm=TRUE),e=e,w=w),by=list(x,by),.SDcols=c("y","w","e")]
+    data.hist <- data.plot[,sum(e),by=list(x,by)][,freq:=round(V1/sum(V1)*100,1)][order(by,x)]
+    
+    resPlot <- plotly::plot_ly(data=data.agg,x=~x,y=~y,color=~paste0("Observed: ",by)) %>%
+        plotly::add_lines(yaxis="y1") %>%
+        plotly::add_bars(x=~x, y=~freq, color=~paste0("Observed: ",by),colors='Set1', data=data.hist,
                           marker=list(line=list(color="#606060", width=1.5)),
                           showlegend=FALSE, opacity=0.5, yaxis="y2") %>%
-        plotly::layout(title=strTitle, legend=l, barmode="stack",
-                       xaxis=ax, yaxis=ay1, yaxis2=ay2)
-    )
+        plotly::layout(title=strTitle, xaxis=ax, yaxis=ay1, yaxis2=ay2, 
+                       legend=l, barmode="stack", margin=list(l=-5,r=-5,t=-5,b=-5))
+
+    suppressWarnings(resPlot)
   }
 }
