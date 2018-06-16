97aca5b183b09683a1bf56c5016e0e0e7c8990f4
Sixiang Hu <Sixiang.Hu@Gmail.com>
2018-06-16 22:43:23 +0100

    + improve efficiency of datasummary + return mode as mean for character variable

--- R/DataSummary.R
+++ R/DataSummary.R
@@ -3,21 +3,23 @@
 #' @description function gives summary of the dataset.
 #' @usage DataSummary(data,missing=list(NA),wt=NULL,entropy=FALSE,
 #' entropy_method="emp",sparkline=FALSE)
-#' @param data This could be data frame or a vector.
-#' @param missing list of possible values stand for missing.
-#' @param wt For a sampled dataset, you may want to specify the wieght for stats calcualted.  
-#' It can be a character which is a column name in the dataset provided, or integer (numeric) weights vector.
-#' @param entropy logical. whether to include entropy as part of output. It will take quite a lot of time.
+#' @param data This could be data frame, a vector, matrix or data.table.
+#' @param missing list of possible missing values.
+#' @param wt For a sampled dataset, you may want to specify the wieght for those stats calcualted.  
+#'     It can be a character which is a column name in the dataset provided, or integer (numeric) weights vector.
+#' @param entropy logical. whether to include entropy as part of output. It will take quite a long time.
 #' @param entropy_method The name of the entropy estimator. 
-#' The `infotheo` package implements four estimators : "emp", "mm", "shrink", "sg" (default:"emp").
+#'     The `infotheo` package implements four estimators : "emp", "mm", "shrink", "sg" (default:"emp").
 #' @param sparkline logical. If true, a string of level percentage will be generated, 
-#' which can be used later in `shiny` app with `sparkline` package.
+#'     which can be used later in `shiny` app with `sparkline` package.
 #' @details This function provides a data summary including min, max, number of unique values and number if missing values.
-#' The min and max will ignore missing value in the data.  The input should be a `data.frame`.
+#'     The min and max will ignore missing value in the data.
+#'     As for the mean value, if it is character variable the mode (most frequency level) will be recorded.
 #' @author Sixiang Hu
-#' @importFrom data.table data.table := setDT
+#' @importFrom data.table data.table := setDT uniqueN
 #' @importFrom infotheo entropy discretize
 #' @importFrom stats weighted.mean
+#' @checkmate testCharacter testNumeric
 #' @export DataSummary
 #' @examples
 #' DataSummary(mtcars,missing=list(NA,".","Unknown",-1))
@@ -40,9 +42,9 @@ DataSummary.data.frame <- function(data,missing=list(NA),wt=NULL,
 DataSummary.data.table <- function(data,missing=list(NA),wt=NULL,
                                    entropy=FALSE,entropy_method="emp",sparkline=FALSE){
   
-  if (is.null(wt)) weight <- rep(1,nrow(data))
-  else if ( is.character(wt) ) weight <- data[[wt]]
-  else if ( is.numeric(wt) &&  length(wt) == nrow(data)) weight <- wt
+  if(is.null(wt)) weight <- rep(1,nrow(data))
+  else if ( testCharacter(wt) ) weight <- data[[wt]]
+  else if ( testNumeric(wt) &&  length(wt) == nrow(data)) weight <- wt
   else stop("DataSummary: wt provided is not in correct format.\n")
   
   nr <- nrow(data)
@@ -51,41 +53,40 @@ DataSummary.data.table <- function(data,missing=list(NA),wt=NULL,
   for (i in dsName) set(data,i=which(data[[i]] %in% missing), j=i, value=NA)
      
   dsClass   <- data[,sapply(.SD,function(x) class(x)[1])]
-  dsNLevels <- data[,sapply(.SD,function(x) data.table::uniqueN(x))]
+  dsNLevels <- data[,sapply(.SD,function(x) uniqueN(x))]
   dsMiss    <- data[,sapply(.SD,function(x) sum(is.na(x)))]
 
-  dsMean    <- data[,sapply(.SD,function(x){
-    if( is.numeric(x) ) { as.character(round(weighted.mean(x,weight,na.rm = TRUE),6))}
-    else {"NA"}
-  })]
-
-  dsMax    <- data[,sapply(.SD,function(x){
-    if(is.numeric(x)) {as.character(round(max(x,na.rm = TRUE),6))}
-    else {
-      x.dt<-data.table::data.table(x,weight)
-      dsTemp <- as.character(x.dt[,sum(weight),by=x][order(-V1)][1,list(x)])
-      if(is.null(dsTemp) || is.na(dsTemp)) dsTemp <- as.character(x.dt[,sum(weight),by=x][order(-V1)][2,list(x)])
-      return(dsTemp)
+  dsMaxMin    <- data[,sapply(.SD,function(x,weight){
+    if(testNumeric(x)) {
+      dsTempMax <- as.character(round(max(x,na.rm = TRUE),6))
+      dsTempMin <- as.character(round(min(x,na.rm = TRUE),6))
+    }else {
+      x.dt<- data.table(x,weight)
+      dsFreq <- x.dt[,sum(weight),by=x][order(-V1)]
+      dsTempMax <- as.character(dsFreq[1,list(x)])
+      dsTempMin <- as.character(dsFreq[nrow(dsFreq),list(x)])
+      if(dsTempMax=="NA" || is.na(dsTempMax)) dsTempMax <- as.character(dsFreq[2,list(x)])
+      if(dsTempMin=="NA" || is.na(dsTempMin)) dsTempMin <- as.character(dsFreq[nrow(dsFreq)-1,list(x)])
     }
-  })]
+    return(paste0(dsTempMax,"|DM|",dsTempMin))
+  },weight=weight)]
   
-  dsMin    <- data[,sapply(data,function(x){
-    if(is.numeric(x)) as.character(round(min(x,na.rm = TRUE),6))
-    else {
-      x.dt<-data.table::data.table(x,weight)
-      dsTemp <- as.character(x.dt[,sum(weight),by=x][order(V1)][1,list(x)])
-      if(is.null(dsTemp) || is.na(dsTemp)) dsTemp <- as.character(x.dt[,sum(weight),by=x][order(V1)][2,list(x)])
-      return(dsTemp)
-    }
-  })]
+  dsMean    <- data[,sapply(.SD,function(x,weight){
+    if( testNumeric(x) ) { as.character(round(sum(x*weight,na.rm = TRUE)/sum(weight,na.rm = TRUE),6))}
+    else {"NA"}
+  },weight=weight)]
+  
+  dsMax <- gsub("(.*)(\\|DM\\|)(.*)","\\1",dsMaxMin)
+  dsMin <- gsub("(.*)(\\|DM\\|)(.*)","\\3",dsMaxMin)
+  dsMean <- ifelse(is.na(dsMean) | dsMean=="NA",dsMax,dsMean)
   
   dsEntropy <- rep(0,length(dsName))
-  if(entropy) dsEntropy <- data[,sapply(data,function(x) infotheo::entropy(infotheo::discretize(x), method = entropy_method))]
+  if(entropy) dsEntropy <- data[,sapply(data,function(x) entropy(discretize(x), method = entropy_method))]
 
-  if (sparkline) {
+  if(sparkline) {
     dsStr <- data[,sapply(data,function(x) {
-      freq <- data.table::data.table(x)[,.N,by=x][order(x)][,f:=N/sum(N)]
-      if (dim(freq)[1]>=100) return("More than 100 levels")
+      freq <- data.table(x)[,.N,by=x][order(x)][,f:=N/sum(N)]
+      if (nrow(freq)>=100) return("More than 100 levels")
       else return(paste0(round(freq[,f],3),collapse= ","))
     })]
     
@@ -98,8 +99,7 @@ DataSummary.data.table <- function(data,missing=list(NA),wt=NULL,
                       "Min"=dsMin,
                       "Max"=dsMax,
                       "Entropy"=round(dsEntropy,6),
-                      "%Distribution"=dsStr,
-                      stringsAsFactors = FALSE))
+                      "%Distribution"=dsStr))
   }else{
     return(data.table("VarName"=dsName,
                       "VarType"=dsClass,
@@ -109,7 +109,6 @@ DataSummary.data.table <- function(data,missing=list(NA),wt=NULL,
                       "Mean"=dsMean,
                       "Min"=dsMin,
                       "Max"=dsMax,
-                      "Entropy"=round(dsEntropy,6),
-                      stringsAsFactors = FALSE))
+                      "Entropy"=round(dsEntropy,6)))
   }
 }
